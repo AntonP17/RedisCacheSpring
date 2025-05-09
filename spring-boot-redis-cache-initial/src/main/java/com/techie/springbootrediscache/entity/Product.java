872d7c2d38919fd6a02a08a792b97106e72870e0
AntonP17 <pokrovskii.anton@mail.ru>
2025-05-09 21:50:05 +0300

    cache redis

---
+++ spring-boot-redis-cache-initial/src/main/java/com/techie/springbootrediscache/entity/Product.java
@@ -0,0 +1,38 @@
+package com.techie.springbootrediscache.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private BigDecimal price;
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
