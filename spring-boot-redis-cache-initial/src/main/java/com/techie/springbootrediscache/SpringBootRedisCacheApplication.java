488b5ad7a07b081a0b357053f049f11f3467e5f5
AntonP17 <pokrovskii.anton@mail.ru>
2025-05-10 22:27:42 +0300

    refractor

--- spring-boot-redis-cache-initial/src/main/java/com/techie/springbootrediscache/SpringBootRedisCacheApplication.java
+++ spring-boot-redis-cache-initial/src/main/java/com/techie/springbootrediscache/SpringBootRedisCacheApplication.java
@@ -2,10 +2,8 @@ package com.techie.springbootrediscache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class SpringBootRedisCacheApplication {
 
     public static void main(String[] args) {
--- spring-boot-redis-cache-initial/src/main/java/com/techie/springbootrediscache/config/RedisConfig.java
+++ spring-boot-redis-cache-initial/src/main/java/com/techie/springbootrediscache/config/RedisConfig.java
@@ -1,18 +1,19 @@
 package com.techie.springbootrediscache.config;
 
 import com.techie.springbootrediscache.dto.ProductDto;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
 
     @Bean
